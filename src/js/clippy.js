db989aa13afb2179a5cd41cb34a6695dcabec832
russellgoldenberg <russellgoldenberg@gmail.com>
2018-06-25 14:56:40 -0400

    fixed clippy

--- src/js/clippy.js
+++ src/js/clippy.js
@@ -1,15 +1,13 @@
 // D3 is included by globally by default
 
 // selections
-const clippy = d3.select('.clippy-character')
-const clickable = d3.selectAll('[data-clippy]')
-
-
+const clippy = d3.select('.clippy-character');
 function resize() {}
 
 function init() {
-	console.log({clickable});
-  console.log("clippy ran")
+	const clickable = d3.selectAll('[data-clippy]');
+	console.log({ clickable });
+	console.log('clippy ran');
 }
 
 export default { init, resize };
