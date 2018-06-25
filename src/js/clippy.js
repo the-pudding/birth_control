ff57ed3ad93fe31e0359ed78d597236b3874673d
Amber Thomas <ProQuestionAsker@gmail.com>
2018-06-25 15:55:33 -0700

    Working on clippy info

--- src/js/clippy.js
+++ src/js/clippy.js
@@ -2,10 +2,51 @@
 
 // selections
 const clippy = d3.select('.clippy-character');
+const info = d3.select('.info-container')
+const dim = d3.select('.clippy-dim')
+const close = d3.select('.close-info')
+
+let copy = null
+
 function resize() {}
 
+function handleClick(){
+  const sel = d3.select(this)
+  const name = sel.at('data-clippy').toLowerCase()
+
+  info
+    .classed('active', true)
+
+  clippy
+    .classed('hiding', false)
+
+  dim
+    .classed('active', true)
+  console.log({sel, name})
+}
+
+function closeInfo(){
+  info
+    .classed('active', false)
+
+  clippy
+    .classed('hiding', true)
+
+  dim
+    .classed('active', false)
+}
+
 function init() {
-	const clickable = d3.selectAll('[data-clippy]');
+  d3.loadData('assets/data/methods.json', (err, response) => {
+    copy = response
+    console.log({response})
+  })
+
+	const clickable = d3.selectAll('[data-clippy]')
+    .on('click', handleClick);
+
+  close
+    .on('click', closeInfo)
 	console.log({ clickable });
 }
 
